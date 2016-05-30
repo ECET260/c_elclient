2b93eb857e046e67fb7d627801021970161f76aa
Marco Vedovati <nobody@nohost.com>
2016-05-30 18:36:03 +0200

    retrying download gcc arm

--- install_gcc_arm.sh
+++ install_gcc_arm.sh
@@ -1,6 +1,6 @@
 set -x
 set -e
-if [ ! -e  gcc-arm-none-eabi-5_3-2016q1-20160330-linux ]; then
+if [ ! -e  gcc-arm-none-eabi-5_3-2016q1 ]; then
     wget https://launchpad.net/gcc-arm-embedded/5.0/5-2016-q1-update/+download/gcc-arm-none-eabi-5_3-2016q1-20160330-linux.tar.bz2
     tar xf gcc-arm-none-eabi-5_3-2016q1-20160330-linux.tar.bz2
 fi
